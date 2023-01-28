cb9214ee25d14b5d801d5f4ebaa25313b6408e3c
Pierre Wieser <pwieser@trychlos.org>
2023-01-28 17:57:53 +0100

    Define '_' global Blaze helper

--- src/client/js/blaze.js
+++ src/client/js/blaze.js
@@ -1,9 +1,17 @@
 /*
- * pwix:i18n/src/client/js/startup.js
+ * pwix:i18n/src/client/js/blaze.js
+ *
+ * Register global helpers.
  */
 
-if( Meteor.isClient ){
-    Meteor.startup( function(){
-        //console.log( 'pwix:i18n/src/client/js/startup.js Meteor.startup()' );
-    });
-}
+import { Template } from 'meteor/templating';
+
+import '../../common/js/index.js';
+
+// translation helper
+//  expects two arguments:
+//  - namespace or translations object or function
+//  - key
+Template.registerHelper( '_', function( namespace, key ){
+    return pwixI18n.label( namespace, key );
+});
