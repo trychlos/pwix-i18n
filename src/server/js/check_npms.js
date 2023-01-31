0c036ad03c2be956909c0b5cba1689526413cf86
Pierre Wieser <pwieser@trychlos.org>
2023-01-31 19:31:47 +0100

    Check npm dependencies at runtime

---
+++ src/server/js/check_npms.js
@@ -0,0 +1,9 @@
+/*
+ * pwix:i18n/src/server/js/check_npms.js
+ */
+
+import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
+
+checkNpmVersions({
+    'printf': '^0.6.1'
+}, 'pwix:i18n');
