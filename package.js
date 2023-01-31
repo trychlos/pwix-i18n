4cb4b0ed152178e3f9f2f49ea95a38823bc457a9
Pierre Wieser <pwieser@trychlos.org>
2023-01-31 19:33:30 +0100

    Add tmeasday:check-npm-versions dependency

--- package.js
+++ package.js
@@ -25,6 +25,7 @@ Package.onTest( function( api ){
 function configure( api ){
     api.versionsFrom( '1.8.1' );
     api.use( 'ecmascript' );
+    api.use( 'tmeasday:check-npm-versions@1.0.2' );
 }
 
 // NPM dependencies are checked in /src/server/js/check_npms.js
